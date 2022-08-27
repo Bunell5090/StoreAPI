b330459a90e4b724efa7a6881622094f85290f20
Bunell5090 <97351508+Bunell5090@users.noreply.github.com>
2022-08-27 16:26:32 -0500

    Created user table model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_212329) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_27_212612) do
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -28,6 +28,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_212329) do
     t.string "description"
     t.string "image"
     t.string "seller"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "password"
+    t.string "password_confirmation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
