4c04211f383b94ea730878f31aefd600bb78c5da
Bunell5090 <97351508+Bunell5090@users.noreply.github.com>
2022-08-27 16:24:27 -0500

    Created customer table model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_120542) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_27_212329) do
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "stores", force: :cascade do |t|
     t.string "name"
     t.integer "price"
